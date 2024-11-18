a1f68566544ac8bd6582e728ee4ec234a6dc80e7
Coolcatcastle8 <coolcatcastle43@gmail.com>
2024-11-18 07:09:58 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,120 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "heroWalkSideLeft3":return img`
+. . . . . . 2 2 2 2 2 2 . . . . 
+. . . . 2 2 f f f f f f 2 . . . 
+. . . 2 f f f f f f f f f 2 . . 
+. . . 2 f f f f f f f f f 2 . . 
+. . . 2 f f f f f f f f f f 2 . 
+. . . 2 f f f f f f f f f f 2 . 
+. . 2 f f f f f f f f f f f 2 . 
+. . 2 f f f f f f f f f f f 2 . 
+. . 2 f f f f f f f f f f 2 . . 
+. . . 2 f f f f f f f f 2 . . . 
+. . . . 2 f f f f f f f 2 . . . 
+. . . . . 2 f f f f f f 2 . . . 
+. . . . . 2 f f f f f f 2 . . . 
+. . . . . 2 f f f f f f 2 . . . 
+. . . . . . 2 f f f f 2 . . . . 
+. . . . . . . f f f f . . . . . 
+`;
+            case "image4":
+            case "heroWalkSideLeft1":return img`
+. . . . 2 2 2 2 2 2 . . . . . . 
+. . . 2 f f f f f f 2 2 . . . . 
+. . 2 f f f f f f f f f 2 . . . 
+. . 2 f f f f f f f f f 2 . . . 
+. 2 f f f f f f f f f f 2 . . . 
+. 2 f f f f f f f f f f 2 . . . 
+. 2 f f f f f f f f f f f 2 . . 
+. 2 f f f f f f f f f f f 2 . . 
+. . 2 f f f f f f f f f f 2 . . 
+. . . 2 f f f f f f f f 2 . . . 
+. . . 2 f f f f f f f 2 . . . . 
+. . . 2 f f f f f f 2 . . . . . 
+. . . 2 f f f f f f 2 . . . . . 
+. . . 2 f f f f f f 2 . . . . . 
+. . . . 2 f f f f 2 . . . . . . 
+. . . . . f f f f . . . . . . . 
+`;
+            case "image5":
+            case "heroWalkSideLeft2":return img`
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . 8 f f f f f f 8 8 . . . . 
+. . 8 f f f f f f f f f 8 . . . 
+. . 8 f f f f f f f f f 8 . . . 
+. 8 f f f f f f f f f f 8 . . . 
+. 8 f f f f f f f f f f 8 . . . 
+. 8 f f f f f f f f f f f 8 . . 
+. 8 f f f f f f f f f f f 8 . . 
+. . 8 f f f f f f f f f f 8 . . 
+. . . 8 f f f f f f f f 8 . . . 
+. . . 8 f f f f f f f 8 . . . . 
+. . . 8 f f f f f f 8 . . . . . 
+. . . 8 f f f f f f 8 . . . . . 
+. . . 8 f f f f f f 8 . . . . . 
+. . . . 8 f f f f 8 . . . . . . 
+. . . . . f f f f . . . . . . . 
+`;
+            case "image2":
+            case "heroWalkSideLeft0":return img`
+. . . . . . 8 8 8 8 8 8 . . . . 
+. . . . 8 8 f f f f f f 8 . . . 
+. . . 8 f f f f f f f f f 8 . . 
+. . . 8 f f f f f f f f f 8 . . 
+. . . 8 f f f f f f f f f f 8 . 
+. . . 8 f f f f f f f f f f 8 . 
+. . 8 f f f f f f f f f f f 8 . 
+. . 8 f f f f f f f f f f f 8 . 
+. . 8 f f f f f f f f f f 8 . . 
+. . . 8 f f f f f f f f 8 . . . 
+. . . . 8 f f f f f f f 8 . . . 
+. . . . . 8 f f f f f f 8 . . . 
+. . . . . 8 f f f f f f 8 . . . 
+. . . . . 8 f f f f f f 8 . . . 
+. . . . . . 8 f f f f 8 . . . . 
+. . . . . . . f f f f . . . . . 
+`;
+            case "image3":
+            case "myImage":return img`
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . 2 2 f f f f 2 2 . . . . 
+. . . 2 f f f f f f f f 2 . . . 
+. . 2 f f f f f f f f f f 2 . . 
+. . 2 f f f f f f f f f f 2 . . 
+. 2 f f f f f f f f f f f f 2 . 
+. 2 f f f f f f f f f f f f 2 . 
+. 2 f f f f f f f f f f f f 2 . 
+. 2 f f f f f f f f f f f f 2 . 
+. . 2 f f f f f f f f f f 2 . . 
+. . . 2 f f f f f f f f 2 . . . 
+. . 2 f f f f f f f f f f 2 . . 
+. . 2 f f f f f f f f f f 2 . . 
+. . 2 2 2 f f f f f f 2 2 2 . . 
+. . . . 2 f f f f f f 2 . . . . 
+. . . . 2 f f . . f f 2 . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
